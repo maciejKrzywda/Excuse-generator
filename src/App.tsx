714222e57189b154533a3099bc3b2c6b1519f402
maciej <dupa1@poczta.dobra.pl>
2025-04-25 09:31:55 +0200

    stuff

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,3 @@
-import './App.css'
 import Form from "./Form.tsx";
 
 function App() {
--- src/Form.tsx
+++ src/Form.tsx
@@ -13,24 +13,25 @@ interface FormData {
 
 const Form = () => {
 
-    const veryCreative = ["Kosmici porwali mi psa", "Meteoryt spadł na mój dom"]
-    const Creative = ["Proszę pana pana tam nie było","Podzieliłem przez zero", "Byłem na Ukrainie i walczyłem"]
-    const LittleCreative = ["Byłem w innym kraju","Wybuchł mi dom"]
-    const Standard = ["Nie chciało mi się",]
-    const NotCreative = ["Ojciec kazał mi iść po piwo","Wujek wszedł za głęboko","Ojciec urodził"]
+    const veryCreative = ["kosmici porwali mi psa", "meteoryt spadł na mój dom","zostałem porwany przez kosmitów"]
+    const creative = ["proszę pana pana tam nie było","podzieliłem przez zero", "coś"]
+    const littleCreative = ["byłem w innym kraju","wybuchł mi dom","byłem poza domem"]
+    const standard = ["nie chciało mi się","zaspałem","zapomniałem"]
+    const notCreative = ["something","something else","something completly different"]
 
     const [formData, setFormData] = useState<FormData>({name: "", excuseType: "", beliveabilityLevel: "",date: "",creativityLevel: "",uniqueTextArea: "",importancy: false})
 
     let important = ""
+    let currentExcuse = ""
 
     if (formData.importancy == false) {
-        important = "Nie Pilną"
+        important = "nie pilną"
     }
     else  if (formData.importancy == true) {
-        important = "Pilną"
+        important = "pilną"
     }
     else {
-        important = "Nie Pilną"
+        important = "nie pilną"
     }
 
     const handleChange = (e: React.ChangeEvent<HTMLInputElement |  HTMLSelectElement | HTMLTextAreaElement>) => {
@@ -43,20 +44,21 @@ const Form = () => {
 
     const handleSubmit = (e: React.FormEvent) => {
         e.preventDefault();
-        // if (formData.beliveabilityLevel == "1" && formData.creativityLevel == "nie kreatywna") {
-        //     console.log("Nazwa osoby proszącej: ", formData.name, " Panie Profesorze, niestety", formData.excuseType, ", ponieważ", Standard[0], ", a poziom wiarygodności tej wymówki to:", formData.beliveabilityLevel, ", zdarzenie miało miejsce (", formData.date, "), kreatywność", formData.creativityLevel, ", wymówkę oznaczono jako ", important);
-        // } else if (formData.beliveabilityLevel == "2" && formData.creativityLevel == "nie przeginaj") {
-        //     console.log("Nazwa osoby proszącej: ", formData.name, " Panie Profesorze, niestety", formData.excuseType, ", ponieważ", Standard[0], ", a poziom wiarygodności tej wymówki to:", formData.beliveabilityLevel, ", zdarzenie miało miejsce (", formData.date, "), kreatywność", formData.creativityLevel, ", wymówkę oznaczono jako ", important);
-        // } else if (formData.beliveabilityLevel == "3" && formData.creativityLevel == "lekko kreatywna") {
-        //     console.log("Nazwa osoby proszącej: ", formData.name, " Panie Profesorze, niestety", formData.excuseType, ", ponieważ", Standard[0], ", a poziom wiarygodności tej wymówki to:", formData.beliveabilityLevel, ", zdarzenie miało miejsce (", formData.date, "), kreatywność", formData.creativityLevel, ", wymówkę oznaczono jako ", important);
-        // } else if (formData.beliveabilityLevel == "4" && formData.creativityLevel == "odlotowa") {
-        //     console.log("Nazwa osoby proszącej: ", formData.name, " Panie Profesorze, niestety", formData.excuseType, ", ponieważ", Standard[0], ", a poziom wiarygodności tej wymówki to:", formData.beliveabilityLevel, ", zdarzenie miało miejsce (", formData.date, "), kreatywność", formData.creativityLevel, ", wymówkę oznaczono jako ", important);
-        // } else if (formData.beliveabilityLevel == "5" && formData.creativityLevel == "nie z tego świata") {
-        //     console.log("Nazwa osoby proszącej: ", formData.name, " Panie Profesorze, niestety", formData.excuseType, ", ponieważ", Standard[0], ", a poziom wiarygodności tej wymówki to:", formData.beliveabilityLevel, ", zdarzenie miało miejsce (", formData.date, "), kreatywność", formData.creativityLevel, ", wymówkę oznaczono jako ", important);
-        // } else {
-        //     alert("poziom wiarygodności i kreatywności musi być ten sam")
-        // }
-        console.log("Nazwa osoby proszącej: ", formData.name, " Panie Profesorze, niestety", formData.excuseType, ", ponieważ", Standard[0], ", a poziom wiarygodności tej wymówki to:", formData.beliveabilityLevel, ", zdarzenie miało miejsce (", formData.date, "), kreatywność", formData.creativityLevel, ", wymówkę oznaczono jako ", important);
+        const randomNumber = Math.floor(Math.random()*3)
+        if (formData.beliveabilityLevel == "1" && formData.creativityLevel == "nie kreatywna") {
+            currentExcuse = `Nazwa osoby proszącej:  ${formData.name}\nPanie Profesorze, niestety  ${formData.excuseType} , ponieważ, ${notCreative[randomNumber]} \npoziom wiarygodności tej wymówki to: ${formData.beliveabilityLevel}\nzdarzenie miało miejsce ( ${formData.date} )\nkreatywność:  ${formData.creativityLevel}\nDodatkowe informacje: ${formData.uniqueTextArea}\nwymówkę oznaczono jako ${important}`;
+        } else if (formData.beliveabilityLevel == "2" && formData.creativityLevel == "nie przeginaj") {
+            currentExcuse = `Nazwa osoby proszącej:  ${formData.name}\nPanie Profesorze, niestety  ${formData.excuseType} , ponieważ, ${standard[randomNumber]} \npoziom wiarygodności tej wymówki to: ${formData.beliveabilityLevel}\nzdarzenie miało miejsce ( ${formData.date} )\nkreatywność:  ${formData.creativityLevel}\nDodatkowe informacje: ${formData.uniqueTextArea}\nwymówkę oznaczono jako ${important}`;
+        } else if (formData.beliveabilityLevel == "3" && formData.creativityLevel == "lekko kreatywna") {
+            currentExcuse = `Nazwa osoby proszącej:  ${formData.name}\nPanie Profesorze, niestety  ${formData.excuseType} , ponieważ, ${littleCreative[randomNumber]} \npoziom wiarygodności tej wymówki to: ${formData.beliveabilityLevel}\nzdarzenie miało miejsce ( ${formData.date} )\nkreatywność:  ${formData.creativityLevel}\nDodatkowe informacje: ${formData.uniqueTextArea}\nwymówkę oznaczono jako ${important}`;
+        } else if (formData.beliveabilityLevel == "4" && formData.creativityLevel == "odlotowa") {
+            currentExcuse = `Nazwa osoby proszącej:  ${formData.name}\nPanie Profesorze, niestety  ${formData.excuseType} , ponieważ, ${creative[randomNumber]} \npoziom wiarygodności tej wymówki to: ${formData.beliveabilityLevel}\nzdarzenie miało miejsce ( ${formData.date} )\nkreatywność:  ${formData.creativityLevel}\nDodatkowe informacje: ${formData.uniqueTextArea}\nwymówkę oznaczono jako ${important}`;
+        } else if (formData.beliveabilityLevel == "5" && formData.creativityLevel == "nie z tego świata") {
+            currentExcuse = `Nazwa osoby proszącej:  ${formData.name}\nPanie Profesorze, niestety  ${formData.excuseType} , ponieważ, ${veryCreative[randomNumber]} \npoziom wiarygodności tej wymówki to: ${formData.beliveabilityLevel}\nzdarzenie miało miejsce ( ${formData.date} )\nkreatywność:  ${formData.creativityLevel}\nDodatkowe informacje: ${formData.uniqueTextArea}\nwymówkę oznaczono jako ${important}`;
+        } else {
+            alert("poziom wiarygodności i kreatywności musi być ten sam")
+        }
+        console.log(currentExcuse)
 
     };
 
@@ -67,7 +69,7 @@ const Form = () => {
                 <tbody>
                 <tr>
                     <td className={"tableLeft"}> <label> Imię: </label> </td>
-                    <td className={"tableRight"}> <label> <input type={"text"} name={"name"} value={formData?.name} onChange={handleChange}/> </label> </td>
+                    <td className={"tableRight"}> <label> <input type={"text"} name={"name"} value={formData?.name} onChange={handleChange} required={true}/> </label> </td>
                 </tr>
 
                 <tr>
@@ -75,6 +77,7 @@ const Form = () => {
                     <td className={"tableRight"}>
                         <label>
                             <select name={"excuseType"} value={formData.excuseType} onChange={handleChange}>
+                                <option>--Select--</option>
                                 <option value={"nie dostarczyłem pracy domowej"}>nie dostarczyłem pracy domowej</option>
                                 <option value={"spóźniłem się"}>spóźniłem się</option>
                                 <option value={"zaspałem na lekcje"}>zaspałem na lekcje</option>
@@ -85,19 +88,20 @@ const Form = () => {
 
                 <tr>
                     <td className={"tableLeft"}> <label>Poziom wiarygodności: </label> </td>
-                    <td className={"tableRight"}> <label> <input type={"range"} name={"beliveabilityLevel"} /*is that how it's written*/ min={1} max={5} value={formData?.beliveabilityLevel} onChange={handleChange} /> </label> </td>
+                    <td className={"tableRight"}> <label> <input type={"range"} name={"beliveabilityLevel"} /*is that how it's written*/ min={1} max={5} value={formData?.beliveabilityLevel} onChange={handleChange}/> </label> </td>
                 </tr>
 
                 <tr>
                     <td className={"tableLeft"}> <label> Data zdarzenia: </label> </td>
-                    <td className={"tableRight"}> <label> <input type={"date"} name={"date"} value={formData?.date} onChange={handleChange}/> </label> </td>
+                    <td className={"tableRight"}> <label> <input type={"date"} name={"date"} value={formData?.date} onChange={handleChange} required={true}/> </label> </td>
                 </tr>
 
                 <tr>
                     <td className={"tableLeft"}> <label> Poziom kreatywnośi: </label> </td>
                     <td className={"tableRight"}>
                         <label>
-                            <select name={"CreativityLevel"} value={formData.creativityLevel} onChange={handleChange}>
+                            <select name={"creativityLevel"} value={formData.creativityLevel} onChange={handleChange}>
+                                <option>--Select--</option>
                                 <option value={"nie z tego świata"}>nie z tego świata</option>
                                 <option value={"odlotowa"}>odlotowa</option>
                                 <option value={"lekko kreatywna"}>lekko kreatywna</option>
@@ -110,7 +114,7 @@ const Form = () => {
 
                 <tr>
                     <td className={"tableLeft"}> <label> Dodatkowe szczegóły: </label> </td>
-                    <td className={"tableRight"}> <label> <textarea id={"uniqueTextArea"} onChange={handleChange}/> </label> </td>
+                    <td className={"tableRight"}> <label> <textarea id={"uniqueTextArea"} name={"uniqueTextArea"} value={formData.uniqueTextArea} onChange={handleChange}/> </label> </td>
                 </tr>
 
                 <tr>
@@ -119,6 +123,7 @@ const Form = () => {
                 </tr>
                 <tr>
                     <td className={"tableLeft"}><button type={"submit"}>Stwórz</button></td>
+                    <td className={"tableRight"}>{currentExcuse && <h3>{currentExcuse}</h3>}</td>
                 </tr>
                 </tbody>
             </table>
